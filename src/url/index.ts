0fd3a8391f32faa33c0287ac1bd190feb3a11bc5
yee <1062948995@qq.com>
2025-03-19 14:47:48 +0800

    docs: fix url

--- src/url/index.ts
+++ src/url/index.ts
@@ -2,9 +2,9 @@ import { Recordable } from '@/typing'
 
 /**
  *
+ * 将URL的查询参数解析为对象
  * @group URL
  * @category Parameter
- * 将URL的查询参数解析为对象
  * @param url - 完整的URL字符串（例如：http://example.com?name=John&age=20）
  * @returns 包含所有查询参数键值对的对象（例如：{ name: 'John', age: '20' }）
  * @example
@@ -30,9 +30,10 @@ export function param2Obj(url: string): Recordable {
 }
 
 /**
+ *
+ * 将对象转换为URL查询字符串
  * @group URL
  * @category Parameter
- * 将对象转换为URL查询字符串
  * @param obj - 需要转换的对象（例如：{ name: 'John', age: 20 }）
  * @param excludeKey - 需要排除的键名数组（默认空数组）
  * @param decodeFlag - 是否对结果进行URI解码（默认false）
